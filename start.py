c42b14300daf9505cd467f940b15132ae6691e27
saridewi88 <39361007+saridewi88@users.noreply.github.com>
2018-05-19 07:28:24 -0700

    please check it
    modularization and class project

---
+++ start.py
@@ -0,0 +1,10 @@
+from buku.views import run_view
+from teman.models import daftar_teman
+from buku.models import daftar_buku
+
+as view_teman
+as view_buku
+
+view_teman.run_view()
+view_buku.run_view()
+
